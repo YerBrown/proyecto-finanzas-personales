35ea7802b4f2dabe3bba6936dcad89159c1e8b23
Yerbrown <yerbrown98@gmail.com>
2024-11-14 10:35:25 +0100

    Agregar dockerizacion al proyecto

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,9 +1,23 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-const DB_HOST = 127.0.0.1
-const DB_PORT = 3308
-const DB_USER = user
-const DB_PASSWORD = 12345
-const DB_DATABASE = Proyecto_Finanzas
-const DB_ROOT_PASSWORD = contraseña1
+const DB_HOST = "127.0.0.1";
+const DB_PORT = 3308;
+const DB_USER = user;
+const DB_PASSWORD = 12345;
+const DB_DATABASE = Proyecto_Finanzas;
+const DB_ROOT_PASSWORD = contraseña1;
+
+const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+  dialect: "mysql",
+  host: DB_HOST,
+  port: DB_PORT
+})
+
+async function TestConnection() {
+    try {
+       
+    } catch (error) {
+        
+    }
+}
