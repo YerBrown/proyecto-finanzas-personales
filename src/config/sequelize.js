1224811c291cd0cb0a45c98874dff97e4b23bf46
Yerbrown <yerbrown98@gmail.com>
2024-11-14 10:28:02 +0100

    Arreglar tabla y insert de contenido

---
+++ src/config/sequelize.js
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+
+const DB_HOST = 127.0.0.1
+const DB_PORT = 3308
+const DB_USER = user
+const DB_PASSWORD = 12345
+const DB_DATABASE = Proyecto_Finanzas
+const DB_ROOT_PASSWORD = contraseña1
